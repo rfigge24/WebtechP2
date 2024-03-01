1d426d1b5dd52151bc259834faa2e911e8224ff5
rfigge24 <rubenfigge24@gmail.com>
2024-03-01 15:19:44 +0100

    added " to attributes and changed a css name

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,9 +1,9 @@
 function navMenuClickHandler() {
 var x = document.getElementsByTagName("nav")[0];
-if (x.className === "nav") {
-    x.className += "--menu-open";
+if (x.className === "nav--menu-closed") {
+    x.className = "nav--menu-open";
 } else {
-    x.className = "nav";
+    x.className = "nav--menu-closed";
 }
 }
 
