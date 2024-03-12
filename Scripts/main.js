a73ff4bd7b33cc0ffbafc8d3dd8c46b13a20a684
Cas Bouwman <casbouwman02@gmail.com>
2024-03-12 16:21:21 +0100

    Added content

--- Scripts/main.js
+++ Scripts/main.js
@@ -49,9 +49,7 @@ class book extends creativeWork{
     #cover;
     #plot;
     constructor(title,authors,yearOfCreating,genre,publisher,cover,plot){
-        super(title);
-        super(authors);
-        super(yearOfCreating);
+        super(title,authors,yearOfCreating);
         this.#genre = genre;
         this.#publisher = publisher;
         this.#cover = cover;
@@ -115,8 +113,7 @@ class author extends person{
     #publishedTitles;
     #wikiUrl;
     constructor(name,yearOfBirth,publishedTitles,wikiUrl){
-        super(name);
-        super(yearOfBirth);
+        super(name,yearOfBirth);
         this.#publishedTitles = publishedTitles;
         this.#wikiUrl = wikiUrl;
     }
@@ -167,7 +164,109 @@ function formatPage(){
     var aside = document.querySelectorAll('aside')[0];
     var footer = document.querySelectorAll('footer')[0];
 
+    var main = document.createElement('main');
+    body.appendChild(main);
+    
     var the_circle = new book('The Circle',["David Eggers"],2013,"Science-fiction",'Knopf','https://upload.wikimedia.org/wikipedia/en/2/28/The_Circle_%28Dave_Eggers_novel_-_cover_art%29.jpg',"\"The Circle,\" penned by Dave Eggers, is a gripping tale set in a near-future Silicon Valley, where the power and influence of technology corporations reign supreme. The narrative revolves around Mae Holland, a young and ambitious woman who secures a coveted position at The Circle, a fictional tech company reminiscent of Google or Facebook. As Mae delves deeper into her role, she becomes increasingly enmeshed in the company's culture of transparency and surveillance, blurring the lines between her professional and personal life. Eggers skillfully navigates themes of privacy, ethics, and the consequences of unchecked technological advancement, offering readers a thought-provoking exploration of the perils of a hyper-connected world.");
     
+    var book_info = document.createElement('article');
+    main.appendChild(book_info);
+    var titleNode = document.createElement('section');
+    book_info.appendChild(titleNode);
+    var plotNode = document.createElement('section');
+    book_info.appendChild(plotNode);
+    var infoNode = document.createElement('section');
+    book_info.appendChild(infoNode);
+
+    //Content of Title node
+    var titleHead = document.createElement('h1');
+    titleNode.appendChild(titleHead);
+    var titleText = document.createTextNode(the_circle.title);
+    titleHead.appendChild(titleText);
+
+    var img = document.createElement('IMG');
+    img.src = the_circle.cover;
+    titleNode.appendChild(img);
+
+    //Content of plotnode
+    var plotHead = document.createElement('h2');
+    var plotHeadText = document.createTextNode('Plot');
+    plotHead.appendChild(plotHeadText);
+    plotNode.appendChild(plotHead);
+
+    var plotInfo = document.createElement('p');
+    var plotInfoText = document.createTextNode(the_circle.plot);
+    plotInfo.appendChild(plotInfoText);
+    plotNode.appendChild(plotInfo);
+
+    //Content of infonode
+    var infoHead = document.createElement('h2');
+    var infoHeadText = document.createTextNode('Additional info')
+    infoHead.appendChild(infoHeadText);
+    infoNode.appendChild(infoHead);
+
+    var infoTable = document.createElement('TABLE');
+    infoNode.appendChild(infoTable);
+    var titleTable = document.createElement('TR');
+    var authorTable = document.createElement('TR');
+    var genreTable = document.createElement('TR');
+    var publisherTable = document.createElement('TR');
+    var publicationYearTable = document.createElement('TR');
+
+    infoTable.appendChild(titleTable);
+    infoTable.appendChild(authorTable);
+    infoTable.appendChild(genreTable);
+    infoTable.appendChild(publisherTable);
+    infoTable.appendChild(publicationYearTable);
+
+    var titleTableHead = document.createElement('TD');
+    var titleTableHeadText = document.createTextNode('Title');
+    titleTableHead.appendChild(titleTableHeadText);
+    titleTable.appendChild(titleTableHead);
+
+    var titleTableText = document.createElement('TD');
+    var titleTextHead = document.createTextNode(the_circle.title);
+    titleTableText.appendChild(titleTextHead);
+    titleTable.appendChild(titleTableText);
+
+    var authorTableHead = document.createElement('TD');
+    var authorTableHeadText = document.createTextNode('Author');
+    authorTableHead.appendChild(authorTableHeadText);
+    authorTable.appendChild(authorTableHead);
+    
+    var authorHead = document.createElement('TD');
+    var authorHeadText = document.createTextNode(the_circle.authors);
+    authorHead.appendChild(authorHeadText);
+    authorTable.appendChild(authorHead);
+
+    var genreTableHead = document.createElement('TD');
+    var genreTableHeadText = document.createTextNode('Genre');
+    genreTableHead.appendChild(genreTableHeadText);
+    genreTable.appendChild(genreTableHead);
+
+    var genreTableText = document.createElement('TD');
+    var genreTextHead = document.createTextNode(the_circle.genre);
+    genreTableText.appendChild(genreTextHead);
+    genreTable.appendChild(genreTableText);
+
+    var publisherTableHead = document.createElement('TD');
+    var publisherTableHeadText = document.createTextNode('Publisher');
+    publisherTableHead.appendChild(publisherTableHeadText);
+    publisherTable.appendChild(publisherTableHead);
+
+    var publisherTableText = document.createElement('TD');
+    var publisherTextHead = document.createTextNode(the_circle.publisher);
+    publisherTableText.appendChild(publisherTextHead);
+    publisherTable.appendChild(publisherTableText);
+
+        var publicationYearTableHead = document.createElement('TD');
+    var publicationYearTableHeadText = document.createTextNode('Year of publication');
+    publicationYearTableHead.appendChild(publicationYearTableHeadText);
+    publicationYearTable.appendChild(publicationYearTableHead);
+
+    var publicationYearTableText = document.createElement('TD');
+    var publicationYearTextHead = document.createTextNode(the_circle.yearOfCreating);
+    publicationYearTableText.appendChild(publicationYearTextHead);
+    publicationYearTable.appendChild(publicationYearTableText);
 }
 
