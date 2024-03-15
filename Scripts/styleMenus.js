e954d4130d990b23bf6341bba2e3f0ec26cc5348
rfigge24 <rubenfigge24@gmail.com>
2024-03-15 16:25:43 +0100

    cleaned the stylemenu code and added to all pages.

--- Scripts/styleMenus.js
+++ Scripts/styleMenus.js
@@ -1,4 +1,4 @@
-var styleClassNames = ['default','⚠fontsize-micro⚠','fontsize-8px','fontsize-16px', 'fontsize-24px', 'textcolor-blue', 'textcolor-red', 'textcolor-green', 'textcolor-magenta', 'textcolor-cyan', 'textcolor-brown'];
+var styleClassNames = ['default','fontsize-micro⚠','fontsize-8px','fontsize-16px', 'fontsize-24px', 'textcolor-blue', 'textcolor-red', 'textcolor-green', 'textcolor-magenta', 'textcolor-cyan', 'textcolor-brown'];
 var elementsToStyle;
 var footer;
 var selector1;
@@ -7,10 +7,10 @@ var selector2;
 var selectedElement;
 var selectedStyle;
 
-window.addEventListener("load", myFunction, false)
+window.addEventListener("load", menuInitializer, false);
 
-
-function myFunction(){
+// Function that initializes the selector menus in the footer and call relevant functions:
+function menuInitializer(){
     var tagQuerie = ['body', 'nav', 'header', 'footer', 'aside', 'article', 'section'];
     // Traversing the dom tree using Depth-first search to find all elements corresponding to a tag in our querie:
     elementsToStyle = domTraverser(document.body, tagQuerie, []);
@@ -60,7 +60,7 @@ function domTraverser(node, tagList, matchedNodes){
     return matchedNodes;
 }
 
-
+// Function to dinamically create and add options to the selector menu:
 function optionFactory(idx, optionText, selector){
     var option = document.createElement('option');
     option.value = idx;     //The value is the index of the option in the arrays created before.
@@ -69,14 +69,17 @@ function optionFactory(idx, optionText, selector){
     selector.appendChild(option);
 }
 
+// Eventhandler for the Element Selector Menu:
 function changeSelectedElementOption(){
     var selectedIndex = selector1.selectedIndex;
     var selectedOption = selector1.options[selectedIndex];
     var selectedValue = selectedOption.value;
+
+    // Set the selected element in the global variable:
     selectedElement = elementsToStyle[selectedValue];
 
-
-    for (let i = 0; i < styleClassNames.length; i++) {
+    // Sets the corresponding selected style to the last style in the style list that is on for this element:
+    for (let i = styleClassNames.length; i>=0; i--) {
         let className = styleClassNames[i];
         if (selectedElement.classList.contains(className)) {
             selector2.selectedIndex = i;
@@ -86,18 +89,30 @@ function changeSelectedElementOption(){
     }
 }
 
+//Eventhandler for the Style Selector Menu:
 function changeSelectedStyleOption(){
     var selectedIndex = selector2.selectedIndex;
     var selectedOption = selector2.options[selectedIndex];
     var selectedValue = selectedOption.value;
+    var selectedName = selectedOption.text;
+    var prefix = selectedName.split(' ')[1].split('-')[0];      // first removes the '#.) ' from the selectedName then splits on '-' to get the prefix if there is any.
+    console.log(prefix);
 
-    //remove existing classnames from selectedElementOption
-    styleClassNames.forEach(function(className) {
-        if (selectedElement.classList.contains(className)) {
-            selectedElement.classList.remove(className);
-        }
-    })
+    // Remove existing classnames from selectedElementOption:
+    removeClassNames(selectedElement, prefix);
 
+    // retrieve the selected style and add the corresponding class name to the element:
     selectedStyle = styleClassNames[selectedValue];
     selectedElement.classList.add(selectedStyle);
+}
+
+//Helper Function that removes Class Names from a selectedElement (type can be the default (first array style array element) or any prefix before a '-' in the option text):
+function removeClassNames(selectedElement, type) {
+    styleClassNames.forEach(function(className) {
+        // If type is "default", remove all classNames:
+        // Otherwise, remove only classNames that start with the given type:
+        if (type === styleClassNames[0] || className.startsWith(type)) {
+            selectedElement.classList.remove(className);
+        }
+    });
 }
