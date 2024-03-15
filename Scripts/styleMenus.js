496d875728f373a412e1f5dd6ce8fa0055749d44
rfigge24 <rubenfigge24@gmail.com>
2024-03-15 15:32:31 +0100

    styleMenus are working now

--- Scripts/styleMenus.js
+++ Scripts/styleMenus.js
@@ -1,39 +1,51 @@
-// Defining the tag queries we look for together with a list of styling options:  
-let tagQuerie = ['body', 'header', 'footer', 'aside', 'article', 'section'];
-let styles = ['fontsize-16px', 'fontsize-32px', 'textcolor-black', 'textcolor-red'];
+var styleClassNames = ['default','⚠fontsize-micro⚠','fontsize-8px','fontsize-16px', 'fontsize-24px', 'textcolor-blue', 'textcolor-red', 'textcolor-green', 'textcolor-magenta', 'textcolor-cyan', 'textcolor-brown'];
+var elementsToStyle;
+var footer;
+var selector1;
+var selector2;
+
+var selectedElement;
+var selectedStyle;
 
 window.addEventListener("load", myFunction, false)
 
 
 function myFunction(){
-
+    var tagQuerie = ['body', 'nav', 'header', 'footer', 'aside', 'article', 'section'];
     // Traversing the dom tree using Depth-first search to find all elements corresponding to a tag in our querie:
-    var elementsToStyle = domTraverser(document.body, tagQuerie, []);
+    elementsToStyle = domTraverser(document.body, tagQuerie, []);
 
     // Creating the selectors to add to our footer and adding the attributes:
-    var footer = document.querySelector('footer');
-    var select1 = document.createElement('select');
-    var select2 = document.createElement('select');
+    footer = document.querySelector('footer');
+    selector1 = document.createElement('select');
+    selector2 = document.createElement('select');
 
-    select1.setAttribute("name", "elementSelector");
-    select1.setAttribute("id", "element-selector");  
-    select2.setAttribute("name", "styleSelector");
-    select2.setAttribute("id", "style-selector");
+    selector1.setAttribute("name", "elementSelector");
+    selector1.setAttribute("id", "element-selector");  
+    selector2.setAttribute("name", "styleSelector");
+    selector2.setAttribute("id", "style-selector");
 
     //using our optionFactory function to create options for each element in our arrays:
     for (const [idx, element] of elementsToStyle.entries()) {
-        optionFactory(idx, element.tagName, select1);
+        element.classList.add(styleClassNames[0]);
+        optionFactory(idx, element.tagName, selector1);
     }
+    footer.appendChild(selector1);
+    changeSelectedElementOption();      //initilising the selectedElementOption
+    
 
-    for (const [idx, styling] of styles.entries()){
-        optionFactory(idx, styling, select2);
+    for (const [idx, styling] of styleClassNames.entries()){
+        optionFactory(idx, styling, selector2);
     }
+    footer.appendChild(selector2);
+    changeSelectedStyleOption();        //initilising the selectedStyleOption
 
-    footer.appendChild(select1);
-    footer.appendChild(select2);
+    // Adding the EventListners to the selector elements:
+    selector1.addEventListener("input", changeSelectedElementOption, false);
+    selector2.addEventListener("input", changeSelectedStyleOption, false);
 }
 
-
+// Function to traverse the DOM-tree and returns an array with the Elements in the dom of whom the tagName is in the tagList:
 function domTraverser(node, tagList, matchedNodes){
     // Check if the current node is inside our list of tags:
     if (tagList.includes(node.tagName.toLowerCase())){
@@ -49,10 +61,43 @@ function domTraverser(node, tagList, matchedNodes){
 }
 
 
-function optionFactory(idx, optionText, select){
+function optionFactory(idx, optionText, selector){
     var option = document.createElement('option');
     option.value = idx;     //The value is the index of the option in the arrays created before.
     var text = document.createTextNode(idx + ".) " + optionText);
     option.appendChild(text);
-    select.appendChild(option);
+    selector.appendChild(option);
+}
+
+function changeSelectedElementOption(){
+    var selectedIndex = selector1.selectedIndex;
+    var selectedOption = selector1.options[selectedIndex];
+    var selectedValue = selectedOption.value;
+    selectedElement = elementsToStyle[selectedValue];
+
+
+    for (let i = 0; i < styleClassNames.length; i++) {
+        let className = styleClassNames[i];
+        if (selectedElement.classList.contains(className)) {
+            selector2.selectedIndex = i;
+            selectedStyle = styleClassNames[i];
+            break;
+        }
+    }
+}
+
+function changeSelectedStyleOption(){
+    var selectedIndex = selector2.selectedIndex;
+    var selectedOption = selector2.options[selectedIndex];
+    var selectedValue = selectedOption.value;
+
+    //remove existing classnames from selectedElementOption
+    styleClassNames.forEach(function(className) {
+        if (selectedElement.classList.contains(className)) {
+            selectedElement.classList.remove(className);
+        }
+    })
+
+    selectedStyle = styleClassNames[selectedValue];
+    selectedElement.classList.add(selectedStyle);
 }
